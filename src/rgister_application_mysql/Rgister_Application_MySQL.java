cacafe506489ac9a768c058ad8da5eabd0e2c31d
Manoj-113 <66235470+Manoj-113@users.noreply.github.com>
2021-05-24 12:58:36 +0530

    Start the Register application connection with MySQL connection

---
+++ src/rgister_application_mysql/Rgister_Application_MySQL.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rgister_application_mysql;
+
+/**
+ *
+ * @author SUCCESS
+ */
+public class Rgister_Application_MySQL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
